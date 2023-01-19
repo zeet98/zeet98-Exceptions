862655f1121001f3afce444201097029525cc6fb
tedbeast <theodore.balashov@revature.com>
2023-01-19 14:43:56 -0500

    added useful comment

--- src/main/java/ExceptionChallenge.java
+++ src/main/java/ExceptionChallenge.java
@@ -7,12 +7,16 @@ public class ExceptionChallenge {
      * fires, or you can use a throws declaration to pass on an exception to whatever called the method.
      * (if an exception is thrown from main, Java will crash.) Correctly managing and throwing exceptions actually
      * makes our application more robust as we can't expect operations on files, databases, or the internet to always
-     * succeed.
+     * succeed. For instance, if the development of a new method would require a FileNotFoundException to be thrown in
+     * a certain situation, the line throw new FileNotFoundException();
      *
-     * Write a method to throw an exception (you will also need to add a 'throws' declaration to allow the method
-     * to compile. This notifies Java that this method is *capable* of throwing an exception.) Notice also that this
-     * method has a void return type - it doesn't need to return anything since the test cases are just checking
+     * Write a method that throws an Exception (you will also need to add a 'throws Exception' declaration to allow the
+     * method to compile. This notifies Java that this method is *capable* of throwing an exception.) Notice also that
+     * this method has a void return type - it doesn't need to return anything since the test cases are just checking
      * for a thrown exception!
+     *
+     * This also means that you should NOT write a try/catch block in this method, as the tests are expecting to have
+     * a method thrown to it. A try/catch block would handle the exception within the method.
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
